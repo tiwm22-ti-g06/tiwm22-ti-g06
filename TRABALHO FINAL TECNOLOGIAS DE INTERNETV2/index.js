c365473dfbdec1e53c4b0c6d407d7edb7532796c
diogo-trabalho <diogo.trabalho62296@gmail.com>
2023-01-13 12:58:05 +0000

    general changes
    added image slider, made changes to the background, added some new images, changed some styles

--- TRABALHO FINAL TECNOLOGIAS DE INTERNETV2/index.js
+++ TRABALHO FINAL TECNOLOGIAS DE INTERNETV2/index.js
@@ -11,7 +11,6 @@ $('.hero').mousewheel(function(e){
 $(window).mousewheel(function(e){
 	if( $('.hero.scrolled').length ){
 		if( $(window).scrollTop() == 0 && e.deltaY > 0 ){
-			$('.hero, .content').removeClass('scrolled');
 		}
 	}
 });
